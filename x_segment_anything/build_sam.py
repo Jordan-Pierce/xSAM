30c4d26a20463f368d1dd280fbfacb9d17014685
Jordan Pierce <115024024+Jordan-Pierce@users.noreply.github.com>
2024-12-09 11:03:17 -0500

    Update build_sam.py
    CoralSCOP

--- x_segment_anything/build_sam.py
+++ x_segment_anything/build_sam.py
@@ -10,9 +10,10 @@ from timm.models import create_model
 from functools import partial
 
 from .modeling import ImageEncoderViT, MaskDecoder, PromptEncoder, Sam, TwoWayTransformer
-from .modeling import TinyViT  # MobileSAM
-from .modeling import RepViT   # EdgeSAM
-from .modeling import repvit   # RepViTSAM
+from .modeling import TinyViT               # MobileSAM
+from .modeling import RepViT                # EdgeSAM
+from .modeling import repvit                # RepViTSAM
+from .modeling import MaskDecoderCoralSCOP  # CoralSCOP
 
 prompt_embed_dim = 256
 image_size = 1024
@@ -143,6 +144,18 @@ def build_sam_repvit(checkpoint=None):
     return repvit_sam
 
 
+# CoralSCOP
+def build_sam_vit_b_coralscop(checkpoint=None):
+    return _build_sam(
+        encoder_embed_dim=768,
+        encoder_depth=12,
+        encoder_num_heads=12,
+        encoder_global_attn_indexes=[2, 5, 8, 11],
+        cate_num=2,
+        checkpoint=checkpoint,
+    )
+
+
 sam_model_registry = {
     "default": build_edge_sam,
     "vit_h": build_sam_vit_h,
@@ -150,7 +163,8 @@ sam_model_registry = {
     "vit_b": build_sam_vit_b,
     "vit_t": build_sam_vit_t,
     "edge_sam": build_edge_sam,
-    "repvit": partial(build_sam_repvit)
+    "repvit": partial(build_sam_repvit),
+    "vit_b_coralscop": build_sam_vit_b_coralscop
 }
 
 sam_model_urls = {
@@ -161,9 +175,11 @@ sam_model_urls = {
     "edge_sam_3x": "https://huggingface.co/spaces/chongzhou/EdgeSAM/resolve/main/weights/edge_sam_3x.pth",
     "edge_sam": "https://huggingface.co/spaces/chongzhou/EdgeSAM/resolve/main/weights/edge_sam.pth",
     "repvit": "https://huggingface.co/spaces/jameslahm/repvit-sam/resolve/main/repvit_sam.pt",
+    "vit_b_coralscop": ""
 }
 
 
+# SAM
 def _build_sam_encoder(
         encoder_embed_dim,
         encoder_depth,
@@ -219,4 +235,79 @@ def _build_sam(
         with open(checkpoint, "rb") as f:
             state_dict = torch.load(f, map_location="cpu", weights_only=False)
         sam.load_state_dict(state_dict)
+    return sam
+
+
+# CoralSCOP
+def _build_sam_coralscop(
+    encoder_embed_dim,
+    encoder_depth,
+    encoder_num_heads,
+    encoder_global_attn_indexes,
+    cate_num = 2,
+    checkpoint=None,
+):
+    prompt_embed_dim = 256
+    image_size = 1024
+    vit_patch_size = 16
+    image_embedding_size = image_size // vit_patch_size
+    sam = Sam(
+        image_encoder=ImageEncoderViT(
+            depth=encoder_depth,
+            embed_dim=encoder_embed_dim,
+            img_size=image_size,
+            mlp_ratio=4,
+            norm_layer=partial(torch.nn.LayerNorm, eps=1e-6),
+            num_heads=encoder_num_heads,
+            patch_size=vit_patch_size,
+            qkv_bias=True,
+            use_rel_pos=True,
+            global_attn_indexes=encoder_global_attn_indexes,
+            window_size=14,
+            out_chans=prompt_embed_dim,
+        ),
+        prompt_encoder=PromptEncoder(
+            embed_dim=prompt_embed_dim,
+            image_embedding_size=(image_embedding_size, image_embedding_size),
+            input_image_size=(image_size, image_size),
+            mask_in_chans=16,
+        ),
+        mask_decoder=MaskDecoderCoralSCOP(
+            num_multimask_outputs=3,
+            transformer=TwoWayTransformer(
+                depth=2,
+                embedding_dim=prompt_embed_dim,
+                mlp_dim=2048,
+                num_heads=8,
+            ),
+            transformer_dim=prompt_embed_dim,
+            iou_head_depth=3,
+            iou_head_hidden_dim=256,
+            cate_num=cate_num,
+        ),
+        pixel_mean=[123.675, 116.28, 103.53],
+        pixel_std=[58.395, 57.12, 57.375],
+    )
+    sam.eval()
+
+    if os.path.isfile(checkpoint):
+        print("loading from "+checkpoint)
+        with open(checkpoint, "rb") as f:
+            state_dict = torch.load(f)
+        sam.load_state_dict(state_dict, strict=True)
+    elif os.path.isdir(checkpoint):
+        print("loading from " + checkpoint)
+        with open(os.path.join(checkpoint,"image_encoder.pth"),"rb") as f_encoder:
+            state_dict_encoder = torch.load(f_encoder)
+        sam.image_encoder.load_state_dict(state_dict_encoder, strict=True)
+        with open(os.path.join(checkpoint,"prompt_encoder.pth"),"rb") as f_prompt:
+            state_dict_prompt = torch.load(f_prompt)
+        sam.prompt_encoder.load_state_dict(state_dict_prompt, strict=True)
+
+        with open(os.path.join(checkpoint,"mask_decoder.pth"),"rb") as f_decoder:
+            state_dict_decoder = torch.load(f_decoder)
+        sam.mask_decoder.load_state_dict(state_dict_decoder,strict=False)
+    else:
+        print("no checkpoint is provided!")
+
     return sam
