cc384e56dbc60c1c73282ebbf5f02e2579054220
Jordan Pierce <115024024+Jordan-Pierce@users.noreply.github.com>
2024-12-09 12:00:27 -0500

    Update build_sam.py

--- x_segment_anything/build_sam.py
+++ x_segment_anything/build_sam.py
@@ -2,7 +2,8 @@
 # All rights reserved.
 
 # This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+# LICENSE file in the root directory of this source tree
+import os
 
 import torch
 from timm.models import create_model
