3936a48501119a9ae999887cccfbdfdf6c3b0f59
Jordan Pierce <115024024+Jordan-Pierce@users.noreply.github.com>
2025-01-03 14:30:26 -0500

    Bump version: 0.0.7 → 0.0.8

--- x_segment_anything/__init__.py
+++ x_segment_anything/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Jordan Pierce"""
 __email__ = "jordan.pierce@noaa.gov"
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __credits__ = "National Center for Coastal and Ocean Sciences (NCCOS)"
 
 from .build_sam import (
